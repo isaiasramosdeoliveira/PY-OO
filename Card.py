742bfaf191ed498f17337f8440af27300d5d0c65
isaiasramosdeoliveira <isaias.ramos.olive@gmail.com>
2023-05-13 10:04:30 -0300

    new bank created

--- Card.py
+++ Card.py
@@ -1,4 +1,4 @@
-class Bank:
+class Bank_Universal:
     def __init__(self,value, account):
         self.value = value
         self.account = account
@@ -14,6 +14,9 @@ class Bank:
         account.balance = account.balance + value
         print("Depósito feito com sucesso.")
 
+def Bank_Nubank(Bank_Universal):
+    pass
+
 class Account:
     def __init__(self, name, balance):
         self.name = name
@@ -22,7 +25,7 @@ class Account:
     
 
 account = Account("isaias", 1200)
-bank = Bank(500, account)
+bank = Bank_Nubank(500, account)
 
 bank.pay()
 
